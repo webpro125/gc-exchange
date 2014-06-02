9d55d8193205f569ccdbfaf8307b6f4278edabc0
James Stoup <jstoup@thoriumllc.com>
2014-06-02 16:11:34 -0400

    GCE-1 Added test for homepage

---
+++ spec/routing/pages_routing_spec.rb
@@ -0,0 +1,9 @@
+require "spec_helper"
+
+describe PagesController do
+  describe "routing" do
+    it "routes to root" do
+      get('/').should route_to(controller: 'pages', action: 'home')
+    end
+  end
+end
