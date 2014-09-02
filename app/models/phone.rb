d1c7881cefa4dfa1cf2e6d7263076e497766820f
James Stoup <jstoup@thoriumllc.com>
2014-09-02 16:38:10 -0400

    GCE-19 - Formatting nits and changes from codereview

--- app/models/phone.rb
+++ app/models/phone.rb
@@ -11,7 +11,8 @@ class Phone < ActiveRecord::Base
   validates :number, presence: true
 
   protected
-    def set_phone
-      self.number = ApplicationController.helpers.number_to_phone(self.number)
-    end
+
+  def set_phone
+    self.number = ApplicationController.helpers.number_to_phone(self.number)
+  end
 end
