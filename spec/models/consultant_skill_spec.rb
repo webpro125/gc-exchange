57176ad85ebf7202ddf16b5fdaa87dfdd540f876
adrian-sieradzki <asieradzki@thoriumllc.com>
2014-09-04 16:41:08 -0400

    GCE-24- Updated let statements, removed unneeded code.

--- spec/models/consultant_skill_spec.rb
+++ spec/models/consultant_skill_spec.rb
@@ -1,12 +1,10 @@
 require 'spec_helper'
 
 describe ConsultantSkill do
-  before do
-    let!(:consultant) { @consultant = FactoryGirl.create(:confirmed_consultant) }
-    let!(:skill) { @skill = FactoryGirl.create(:skill) }
-    let!(:consultant_skill) { @consultant_skill = ConsultantSkill.new(skill: skill, consultant: consultant) }
-    id = @skill.id
-  end
+
+  let!(:consultant) { @consultant = FactoryGirl.create(:confirmed_consultant) }
+  let!(:skill) { @skill = FactoryGirl.create(:skill) }
+  @consultant_skill = ConsultantSkill.new(skill: skill, consultant: consultant)
 
   subject { @consultant_skill }
 
@@ -15,14 +13,14 @@ describe ConsultantSkill do
   describe 'skill_id' do
     it 'should not be valid' do
       consultant_skill.skill_id = nil
-      expect(consultant_skill).to_not be_valid
+      expect(@consultant_skill).to_not be_valid
     end
   end
 
   describe 'profile_id' do
     it 'should not be valid' do
       consultant_skill.consultant_id = nil
-      expect(consultant_skill).to_not be_valid
+      expect(@consultant_skill).to_not be_valid
     end
   end
 end
