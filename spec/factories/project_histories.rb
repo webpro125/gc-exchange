f8bbec5153add1950b6cda8fdf01f3a4fc7e1388
James Stoup <jstoup@thoriumllc.com>
2014-09-15 13:49:53 -0400

    GCE-59 - Fix build fail

--- spec/factories/project_histories.rb
+++ spec/factories/project_histories.rb
@@ -4,7 +4,7 @@ FactoryGirl.define do
   factory :project_history do
     consultant
     client_company { Faker::Name.name }
-    client_poc_name { Faker::Name.name }
+    client_poc_name { Faker::Name.first_name }
     client_poc_email { Faker::Internet.email(client_poc_name) }
     start_date { 3.years.ago }
     end_date { 6.months.ago }
