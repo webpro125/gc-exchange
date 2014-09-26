f34b8828ef38887a8dccfd2955b8a29956af4bad
adrian-sieradzki <asieradzki@thoriumllc.com>
2014-09-25 22:28:46 -0400

    GCE-65 - Added CompanyMailer Rspec.

--- spec/mailers/company_mailer_spec.rb
+++ spec/mailers/company_mailer_spec.rb
@@ -1,5 +1,20 @@
-require "spec_helper"
+require 'spec_helper'
 
 describe CompanyMailer do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  let(:sales_lead) { FactoryGirl.create(:sales_lead) }
+  let(:mail) { CompanyMailer.company_registration_request(sales_lead) }
+
+  describe 'Company Registration Request' do
+    before do
+      CompanyMailer.company_registration_request(sales_lead).deliver
+    end
+
+    it 'should send an email' do
+      ActionMailer::Base.deliveries.count.should == 1
+    end
+
+    it 'assigns sales_lead' do
+      expect(mail.body.encoded).to match(sales_lead.email)
+    end
+  end
 end
