c7d3b5bcd8cab8327dbfb858e75f1a492abb6e4b
adrian-sieradzki <asieradzki@thoriumllc.com>
2014-10-03 19:57:52 -0400

    GCE-69 - Updated military policies.

--- spec/policies/military_policy_spec.rb
+++ spec/policies/military_policy_spec.rb
@@ -4,9 +4,6 @@ describe MilitaryPolicy do
   subject { MilitaryPolicy.new(consultant, military) }
 
   describe 'for a user' do
-    let!(:branch) { FactoryGirl.create(:branch) }
-    let!(:rank) { FactoryGirl.create(:rank) }
-    let!(:clearance_level) { FactoryGirl.create(:clearance_level) }
     let!(:consultant) { FactoryGirl.create(:confirmed_consultant) }
 
     describe 'for valid military' do
@@ -14,12 +11,12 @@ describe MilitaryPolicy do
 
       it { should permit_action(:create)  }
       it { should permit_action(:update)  }
-      it { should permit_action(:edit)    }
-      it { should permit_action(:show)    }
-      it { should permit_action(:new)     }
 
+      it { should_not permit_action(:new)     }
       it { should_not permit_action(:index)   }
       it { should_not permit_action(:destroy) }
+      it { should_not permit_action(:edit)    }
+      it { should_not permit_action(:show)    }
 
     end
   end
