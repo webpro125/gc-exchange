7fd0802c5b9f47aeefcd34d5097e553acec60cad
Keith Baton <kbaton@thoriumllc.com>
2014-10-30 22:34:06 -0400

    feature/GCE-52 added more specs

--- spec/services/unset_primary_phones_spec.rb
+++ spec/services/unset_primary_phones_spec.rb
@@ -3,8 +3,28 @@ require 'spec_helper'
 describe PhoneUnsetPrimaries do
   subject { PhoneUnsetPrimaries.new(phone) }
 
+  describe 'with valid phone' do
+    let(:phone) { FactoryGirl.create(:phone, primary: true) }
+
+    it 'should return true on save' do
+      expect(subject).to receive(:save).and_return(true)
+      subject.save
+    end
+  end
+
+  describe 'with invalid phone' do
+    let(:phone) { FactoryGirl.create(:phone, primary: false) }
+
+    it 'should return false on save' do
+      phone.number = nil
+      expect(subject).to receive(:save).and_return(false)
+      subject.save
+    end
+  end
+
   describe 'with primary' do
     let(:phone) { FactoryGirl.create(:phone, primary: true) }
+
     it 'should set other phones not primary' do
       expect(phone).to receive(:save!)
       subject.save
@@ -13,6 +33,7 @@ describe PhoneUnsetPrimaries do
 
   describe 'without primary' do
     let(:phone) { FactoryGirl.create(:phone, primary: false) }
+
     it 'should set other phones not primary' do
       Phone.any_instance.should_not_receive(:update)
       subject.save
