0dd5480bbf0aa7c724bff051dc527b3e4b40faea
adrian-sieradzki <asieradzki@thoriumllc.com>
2014-11-11 16:31:06 -0500

    GCE-123 - Updated to add limit.

--- db/migrate/20141111205943_update_consultat_abstract.rb
+++ db/migrate/20141111205943_update_consultat_abstract.rb
@@ -2,7 +2,8 @@ class UpdateConsultatAbstract < ActiveRecord::Migration
   def up
     change_column :consultants, :abstract, :text, limit: 1500
   end
+
   def down
-    change_column :consultants, :abstract, :string
+    change_column :consultants, :abstract, :string, limit: 1500
   end
 end
