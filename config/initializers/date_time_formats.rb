aae49ac7210d50c13563ee67a0c1bfa68a3d20a2
James Stoup <jstoup@thoriumllc.com>
2014-11-11 22:27:51 -0500

    GCE-122 - Changed date formatting

--- config/initializers/date_time_formats.rb
+++ config/initializers/date_time_formats.rb
@@ -1,8 +1,5 @@
 Time::DATE_FORMATS[:month_and_year] = '%B %Y'
-Time::DATE_FORMATS[:default] = Time::DATE_FORMATS[:month_and_year]
 
 DateTime::DATE_FORMATS[:month_and_year] = '%B %Y'
-DateTime::DATE_FORMATS[:default] = DateTime::DATE_FORMATS[:month_and_year]
 
 Date::DATE_FORMATS[:month_and_year] = '%B %Y'
-Date::DATE_FORMATS[:default] = Date::DATE_FORMATS[:month_and_year]
