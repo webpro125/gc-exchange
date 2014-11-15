5b6caadf71cd8533df196bfcffe25b7cd65cd6de
James Stoup <jstoup@thoriumllc.com>
2014-11-15 11:14:00 -0500

    GCE-134 - change simple_form to do optional instead

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -92,7 +92,13 @@ SimpleForm.setup do |config|
   # config.item_wrapper_class = nil
 
   # How the label text should be generated altogether with the required text.
-  # config.label_text = lambda { |label, required| "#{required} #{label}" }
+  config.label_text = lambda do |label, required|
+    if required.blank?
+      "#{label} <span class='optional'>optional</span>"
+    else
+      label
+    end
+  end
 
   # You can define the class to use on all labels. Default is nil.
   config.label_class = 'control-label'
