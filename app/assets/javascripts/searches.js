e1b3b49872c74e8d2fbcc8a946dffac8282fb014
James Stoup <jstoup@thoriumllc.com>
2014-11-23 17:41:31 -0500

    GCE-124 cleaned up some javascript a tad.  Added translations

--- app/assets/javascripts/searches.js
+++ app/assets/javascripts/searches.js
@@ -1,29 +1,38 @@
-$(document).ready(function(){
-  moreless();
-});
+function moreLess() {
+  'use strict';
 
-function moreless() {
-// Hide the extra content initially, using JS so that if JS is disabled, no problemo.
-$('.read-more-content').addClass('hide');
+  // Hide the extra content initially, using JS so that if JS is disabled, no problemo.
+  $('.read-more-content').addClass('hide');
+}
 
-// Set up the toggle.
-$('.read-more-toggle').on('click', function() {
-  $(this).next('.read-more-content').toggleClass('hide');
-
+function toggleText($this) {
   var config = {
-    moreText: "More information",
-    lessText: "Less information"
+    moreText: $this.data('more-text'),
+    lessText: $this.data('less-text')
   };
 
-  var $this = $(this);
-  if ($this.hasClass('less')) {
-    $this.removeClass('less');
-    $this.html(config.moreText);
-  } else {
-    $this.addClass('less');
+  if ($this.hasClass('more')) {
+    $this.removeClass('more');
     $this.html(config.lessText);
+  } else {
+    $this.addClass('more');
+    $this.html(config.moreText);
   }
   return false;
-
-});
 }
+
+$(document).ready(function(){
+  'use strict';
+
+  moreLess();
+  toggleText($('.read-more-toggle'));
+
+  // Set up the toggle.
+  $('.read-more-toggle').on('click', function() {
+    var $this = $(this);
+
+    $this.next('.read-more-content').toggleClass('hide');
+
+    toggleText($this);
+  });
+});
