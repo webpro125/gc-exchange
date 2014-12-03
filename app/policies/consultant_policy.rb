88e2e5357bc51dc606b31eaab81049052ec52dcf
adrian-sieradzki <asieradzki@thoriumllc.com>
2014-12-03 10:23:57 -0500

    GCE-163 -Policy comment.

--- app/policies/consultant_policy.rb
+++ app/policies/consultant_policy.rb
@@ -31,6 +31,8 @@ class ConsultantPolicy < ApplicationPolicy
     user == record
   end
 
+  # Only allow GCES, logged in Company Users who own the file, or logged in Company Users who
+  # access an approved consultant.
   def download?
     gces? || (user.present? && (user == record || record.approved?))
   end
