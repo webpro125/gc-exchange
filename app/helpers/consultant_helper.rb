7f95ac8089151a690f230fd05a56acd7c1be04d8
adrian-sieradzki <asieradzki@thoriumllc.com>
2014-12-10 10:06:19 -0500

    GCE-268 - UPdated per comments.

--- app/helpers/consultant_helper.rb
+++ app/helpers/consultant_helper.rb
@@ -2,4 +2,8 @@ module ConsultantHelper
   def user_is_gces?
     current_user && current_user.gces?
   end
+
+  def consultant_owns_record?
+    current_consultant.id == @consultant.id
+  end
 end
--- spec/support/example_groups/wizard_redirect_example_group.rb
+++
@@ -1,7 +0,0 @@
-shared_examples 'wizard_redirect' do
-  describe 'consultant' do
-    describe 'wizard_redirect' do
-
-    end
-  end
-end
