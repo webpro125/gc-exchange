05eec59f8fe2bba2b18a72880c193ca27e4b098c
adrian-sieradzki <asieradzki@thoriumllc.com>
2014-12-10 11:04:18 -0500

    GCE-268 - Fixed build.

--- app/helpers/consultant_helper.rb
+++ app/helpers/consultant_helper.rb
@@ -4,8 +4,7 @@ module ConsultantHelper
   end
 
   def consultant_owns_record?
-    if current_consultant
-      current_consultant.id == @consultant.id
-    end
+    return unless current_consultant
+    current_consultant.id == @consultant.id
   end
 end
