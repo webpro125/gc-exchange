16898a50d0df67c966f5655056367977c8b1719d
adrian-sieradzki <asieradzki@thoriumllc.com>
2014-12-12 15:34:48 -0500

    GCE-268 - Refactored rspec.

--- spec/helpers/consultant_helper_spec.rb
+++ spec/helpers/consultant_helper_spec.rb
@@ -3,35 +3,33 @@ require 'spec_helper'
 describe ConsultantHelper do
 
   describe 'logged in' do
-    describe 'as GCES user' do
-      describe '#user_is_gces?' do
-        it 'should return true' do
-          helper.stub_chain(:current_user, :gces?).and_return(true)
-          expect(helper.user_is_gces?).to eq true
-        end
+    describe '#user_is_gces?' do
+      it 'should return true' do
+        helper.stub_chain(:current_user, :gces?).and_return(true)
+        expect(helper.user_is_gces?).to eq true
+      end
 
-        it 'should return false' do
-          helper.stub_chain(:current_user, :gces?).and_return(false)
-          expect(helper.user_is_gces?).to eq false
-        end
+      it 'should return false' do
+        helper.stub_chain(:current_user, :gces?).and_return(false)
+        expect(helper.user_is_gces?).to eq false
       end
     end
 
-    describe 'as consultant' do
-      describe '#consultant_owns_record?' do
-        let(:consultant) { FactoryGirl.create(:consultant, :approved) }
+    describe '#consultant_owns_record?' do
+      let(:consultant) { FactoryGirl.create(:consultant, :approved) }
 
-        it 'should return true' do
-          helper.instance_variable_set('@consultant', consultant)
-          helper.stub_chain(:current_consultant, :id).and_return(consultant.id)
-          expect(helper.consultant_owns_record?).to eq true
-        end
+      before do
+        helper.instance_variable_set('@consultant', consultant)
+      end
 
-        it 'should return false' do
-          helper.instance_variable_set('@consultant', consultant)
-          helper.stub_chain(:current_consultant, :id).and_return(consultant.id + 1)
-          expect(helper.consultant_owns_record?).to eq false
-        end
+      it 'should return true' do
+        helper.stub_chain(:current_consultant, :id).and_return(consultant.id)
+        expect(helper.consultant_owns_record?).to eq true
+      end
+
+      it 'should return false' do
+        helper.stub_chain(:current_consultant, :id).and_return(consultant.id + 1)
+        expect(helper.consultant_owns_record?).to eq false
       end
     end
   end
