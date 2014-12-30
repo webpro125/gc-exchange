7bbd6a373520d92e6c29bbd16d7357ab46a65bb0
adrian-sieradzki <asieradzki@thoriumllc.com>
2014-12-30 10:11:24 -0500

    GCE-290 - Added PMI Project Manager Professional certification to list.

--- app/models/certification.rb
+++ app/models/certification.rb
@@ -16,6 +16,8 @@ class Certification < ActiveRecord::Base
                                               label: 'PMI Agine Certification Practitioner' }
   PMI_RISK_PRACTITIONER                   = { code: 'PMI_RISK_PRACTITIONER',
                                               label: 'PMI Risk Practitioner' }
+  PMI_PROJECT_MANAGER_PROFESSIONAL       = { code: 'PMI_PROJECT_MANAGER_PROFESSIONAL',
+                                              label: 'PMI Project Manager Professional' }
   CERTIFIED_PROFESSIONAL_ACCOUNTANT       = { code: 'CERTIFIED_PROFESSIONAL_ACCOUNTANT',
                                               label: 'Certified Professional Accountant' }
   REGISTERED_ARCHITECT                    = { code: 'REGISTERED_ARCHITECT',
@@ -40,9 +42,10 @@ Library (ITIL)' }
                          PROFESSIONAL_ENGINEER_MECHANICAL, PROFESSIONAL_ENGINEER_ELECTRICAL,
                          PROFESSIONAL_ENGINEER_SYSTEMS, PMI_SCHEDULING_PROFESSIONAL,
                          PMI_AGILE_CERT_PRACTITIONER, PMI_RISK_PRACTITIONER,
-                         CERTIFIED_PROFESSIONAL_ACCOUNTANT, REGISTERED_ARCHITECT,
-                         KNOWLEDGE_MANAGER_PROFESSIONAL, EMERGENCY_MANAGEMENT_PROFESSIONAL,
-                         CERTIFIED_ETHICAL_HACKER, INFORMATION_SYSTEMS_AUDITOR, LAWYER,
+                         PMI_PROJECT_MANAGER_PROFESSIONAL, CERTIFIED_PROFESSIONAL_ACCOUNTANT,
+                         REGISTERED_ARCHITECT,KNOWLEDGE_MANAGER_PROFESSIONAL,
+                         EMERGENCY_MANAGEMENT_PROFESSIONAL, CERTIFIED_ETHICAL_HACKER,
+                         INFORMATION_SYSTEMS_AUDITOR, LAWYER,
                          CERTIFIED_PROFESSIONAL_SYSTEMS_ENGINEER,
                          CERTIFIED_PROFESSIONAL_LOGISTICIAN, ITIL].freeze
 end
