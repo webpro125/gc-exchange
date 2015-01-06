721b1b7ca4b54b7ee3305b74ebefb5cdd52887d4
James Stoup <jstoup@thoriumllc.com>
2015-01-06 10:31:54 -0500

    GCE-293 - Fixed helper text

--- app/models/rank.rb
+++ app/models/rank.rb
@@ -11,6 +11,11 @@ class Rank < ActiveRecord::Base
   O8 = { code: 'O8', label: 'O8' }
   O9 = { code: 'O9', label: 'O9' }
   O10 = { code: 'O10', label: 'O10' }
+  W1 = { code: 'W1', label: 'W1' }
+  W2 = { code: 'W2', label: 'W2' }
+  W3 = { code: 'W3', label: 'W3' }
+  W4 = { code: 'W4', label: 'W4' }
+  W5 = { code: 'W5', label: 'W5' }
   E1 = { code: 'E1', label: 'E1' }
   E2 = { code: 'E2', label: 'E2' }
   E3 = { code: 'E3', label: 'E3' }
@@ -21,5 +26,6 @@ class Rank < ActiveRecord::Base
   E8 = { code: 'E8', label: 'E8' }
   E9 = { code: 'E9', label: 'E9' }
 
-  RANK_TYPES = [O1, O2, O3, O4, O5, O6, O7, O8, O9, O10, E1, E2, E3, E4, E5, E6, E7, E8, E9].freeze
+  RANK_TYPES = [O1, O2, O3, O4, O5, O6, O7, O8, O9, O10, W1, W2, W3, W4, W5, E1, E2, E3, E4, E5, E6,
+                E7, E8, E9].freeze
 end
