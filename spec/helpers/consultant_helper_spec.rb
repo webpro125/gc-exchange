25bdbcb795be6fff94ef0d9db079788f58ce5d5b
James Stoup <jstoup@thoriumllc.com>
2015-02-12 22:59:02 -0500

    GCE-362 Make consultant viewable.

--- spec/helpers/consultant_helper_spec.rb
+++ spec/helpers/consultant_helper_spec.rb
@@ -14,24 +14,5 @@ describe ConsultantHelper do
         expect(helper.user_is_gces?).to eq false
       end
     end
-
-    describe '#consultant_owns_record?' do
-      let(:consultant) { FactoryGirl.create(:consultant, :approved) }
-
-      before do
-        helper.instance_variable_set('@consultant', consultant)
-      end
-
-      it 'should return true' do
-        helper.stub_chain(:current_consultant).and_return(consultant)
-        expect(helper.consultant_owns_record?(consultant)).to eq true
-      end
-
-      it 'should return false' do
-        helper.stub_chain(:current_consultant).and_return(
-          FactoryGirl.create(:consultant, :approved))
-        expect(helper.consultant_owns_record?(consultant)).to eq false
-      end
-    end
   end
 end
