850aaf8ccfb03f128a816e0ef287e67698f76f7d
James Stoup <jstoup@thoriumllc.com>
2015-02-17 20:43:46 -0500

    GCE-372 - Updated per code review.

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,9 +2,9 @@ class Project < ActiveRecord::Base
   enum contact_status: [:hired, :not_interested, :not_pursuing, :agreed_to_terms, :rejected_terms]
 
   scope :open, (lambda do
-    where(arel_table[:contact_status].eq(ContactRequest.contact_statuses[:hired])
+    where(arel_table[:contact_status].eq(Project.contact_statuses[:hired])
                                  .or(arel_table[:contact_status].eq(
-                                       ContactRequest.contact_statuses[:rejected_terms])))
+                                       Project.contact_statuses[:rejected_terms])))
   end)
 
   belongs_to :travel_authorization
--- db/migrate/20150128165840_create_projects.rb
+++ db/migrate/20150128165840_create_projects.rb
@@ -3,7 +3,7 @@ class CreateProjects < ActiveRecord::Migration
     create_table :projects do |t|
       t.references :consultant, index: true, null: false
       t.references :user, index: true, null: false
-      t.references :travel_authorization, index: true
+      t.references :travel_authorization, index: true, null: false
       t.date :proposed_start
       t.date :proposed_end
       t.decimal :proposed_rate, precision: 8, scale: 2
