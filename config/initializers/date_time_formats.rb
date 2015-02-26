7b5ac2ae7fbf5634b8754f3e315ff510b19e0df0
Angela Light <angela.see.light@gmail.com>
2015-02-26 14:36:00 -0500

    GCE-390 – remove commas

--- config/initializers/date_time_formats.rb
+++ config/initializers/date_time_formats.rb
@@ -1,5 +1,5 @@
-Time::DATE_FORMATS[:month_day_and_year] = '%B %d, %Y'
+Time::DATE_FORMATS[:month_day_and_year] = '%B %d %Y'
 
-DateTime::DATE_FORMATS[:month_day_and_year] = '%B %d, %Y'
+DateTime::DATE_FORMATS[:month_day_and_year] = '%B %d %Y'
 
-Date::DATE_FORMATS[:month_day_and_year] = '%B %d, %Y'
+Date::DATE_FORMATS[:month_day_and_year] = '%B %d %Y'
