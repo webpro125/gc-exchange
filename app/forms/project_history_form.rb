d21ad25ee8319c7fa396d1fb8c6ee97463575a9a
adrian-sieradzki <asieradzki@thoriumllc.com>
2015-03-10 12:31:57 -0400

    GCE-444 - Removed duplicate download link from profile, added appropriate error message reference.

--- app/forms/project_history_form.rb
+++ app/forms/project_history_form.rb
@@ -23,7 +23,7 @@ class ProjectHistoryForm < Reform::Form
   validates :client_company, length: { in: 3..512 }, presence: true
   validates :client_poc_name, length: { in: 2..256 }, presence: true,
             format: {  with: RegexConstants::Letters::AND_NUMBERS,
-                       message: I18n.t('activerecord.errors.messages.only_letters_numbers') }
+                       message: I18n.t('activerecord.errors.messages.regex.only_letters_numbers') }
   validates :client_poc_email, length: { in: 3..128 }, presence: true,
             format: { with: RegexConstants::EMAIL,
                       message: I18n.t('activerecord.errors.messages.regex.email') }
