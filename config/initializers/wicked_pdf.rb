a55bf69a9e035458176f82c0141600530c1cea3c
James Stoup <jstoup@james.stoup.home>
2015-05-11 08:32:59 -0400

    GCE-473 - Change exe path

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,5 +1,5 @@
 WickedPdf.config = {
   #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
   layout: 'pdf_layout.html',
-  exe_path: '/usr/bin/wkhtmltopdf'
+  exe_path: "#{Rails.root}/bin/wkhtmltopdf.sh"
 }
