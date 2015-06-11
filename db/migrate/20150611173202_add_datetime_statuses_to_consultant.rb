792fe7569e468480e3289f7f23a0a9456cfca1df
adrian-sieradzki <asieradzki@thoriumllc.com>
2015-06-11 14:08:16 -0400

    GCE-486 - Added datetime for consultant statuses.

---
+++ db/migrate/20150611173202_add_datetime_statuses_to_consultant.rb
@@ -0,0 +1,9 @@
+class AddDatetimeStatusesToConsultant < ActiveRecord::Migration
+  def change
+    add_column :consultants, :date_on_hold, :datetime
+    add_column :consultants, :date_pending_approval, :datetime
+    add_column :consultants, :date_in_progress, :datetime
+    add_column :consultants, :date_approved, :datetime
+    add_column :consultants, :date_rejected, :datetime
+  end
+end
