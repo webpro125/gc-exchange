7b996d33b6e90150f7ae0779ee4248dd7695e1fc
Jess Brown <jess@brownwebdesign.com>
2015-12-01 10:55:21 -0500

    fix bug with date validation. using the validates method with a DateTime.now caches the date when first loaded, using a custom validator fixes the trick.
    https://basecamp.com/2984706/projects/9853765/todos/220800780
    
    https://gitter.im/trailblazer/chat?at=565cf45efd59645429ec4982

--- app/forms/project_history_form.rb
+++ app/forms/project_history_form.rb
@@ -17,9 +17,9 @@ class ProjectHistoryForm < Reform::Form
   validates :project_type_id, presence: true
   validates :position_ids, presence: true
   validates :description, length: { in: 3..1_500 }, allow_blank: true
-  validates :start_date, date: { on_or_before: DateTime.now }
-  validates :end_date, date: { on_or_after: :start_date, on_or_before: DateTime.now },
-            allow_blank: true, if: ->() { start_date.present? }
+  validate :start_date_ok?
+  validate :end_date_ok?
+  validates :end_date, date: { on_or_after: :start_date }, allow_blank: true, if: ->() { start_date.present? }
   validates :client_company, length: { in: 3..512 }, presence: true
   validates :client_poc_name, length: { in: 2..256 }, presence: true,
             format: {  with: RegexConstants::Letters::AND_NUMBERS,
@@ -42,5 +42,17 @@ class ProjectHistoryForm < Reform::Form
               }
 
     validates :phone_type_id, presence: true, unless: ->() { number.empty? }
+  end
+
+  def start_date_ok?
+    if start_date.respond_to?(:>) && start_date > DateTime.now
+      errors.add(:start_date, "needs to be before today")
+    end
+  end
+
+  def end_date_ok?
+    if end_date.respond_to?(:>) && end_date > DateTime.now
+      errors.add(:end_date, "needs to be before today")
+    end
   end
 end
