e9214cba1db9b2e43f8b32a0aa821d82a6deea86
Jess Brown <jess@brownwebdesign.com>
2015-12-10 11:06:13 -0500

    add note about $10 GCES fee
    https://basecamp.com/2984706/projects/9853765/todos/221139476

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,6 @@
 class Company < ActiveRecord::Base
   GLOBAL_CONSULTANT_EXCHANGE = 'Global Consultant Exchange'
+  GCES_FEE = 10
 
   belongs_to :owner, class_name: 'User', inverse_of: :owned_company, dependent: :delete
   has_many :users, dependent: :destroy
