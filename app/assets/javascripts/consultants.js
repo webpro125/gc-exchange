4ecff3f9dffd50ff1e23085c98343a4aa595fd70
Jess Brown <jess@brownwebdesign.com>
2016-02-11 15:24:35 -0500

    create an expand functionality on the consultants table
    https://basecamp.com/2984706/projects/9853765/todos/228319045

--- app/assets/javascripts/consultants.js
+++ app/assets/javascripts/consultants.js
@@ -6,4 +6,18 @@ $(document).ready(function() {
     scrollX:    true,
     ajax:       $('#consultants ').data('source')
   });
+
+});
+
+$(document).on("click",".consultant-positions",function() {
+    remaining_positions = $(this).parents("td").find(".remaining-positions")
+    if(remaining_positions.is(":visible")) {
+      $(this).text("More")
+      remaining_positions.hide()
+    }
+    else{
+      $(this).text("Less")
+      remaining_positions.show()
+    }
+    return false;
 });
