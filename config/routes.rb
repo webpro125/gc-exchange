f45bbfb17346b4873fb87f86b55f23423b194d7e
henry_braun <henry.braun125@gmail.com>
2016-02-24 21:18:13 +0100

    admin consultant controller name changed to consultants

--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,7 @@ Rails.application.routes.draw do
   namespace :admin do
     get '/', :to => 'dashboard#index'
     resources :dashboard
-    resources :consultant
+    resources :consultants
   end
   authenticated :admin do
     root :path => 'admin/dashboard', :to => 'admin/dashboard#index', as: :admin_root
