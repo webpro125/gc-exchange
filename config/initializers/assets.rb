88bbd5bb704ae52e4b8a8e5c03b62e68cac36a05
Jess Brown <jess@brownwebdesign.com>
2016-05-16 12:15:49 -0400

    reconfigure assets in pipeline (no longer blindly compile all assets, manually specify assets in manifest file (ie application.scss) and only compile those files and it's dependencies, as is the rails norm)

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,2 +1,11 @@
-Rails.application.config.assets.precompile += ['*.js', '*.css', '**/*.js', '**/*.css']
+#Rails.application.config.assets.precompile += ['*.js', '*.css', '**/*.js', '**/*.css']
+Rails.application.config.assets.precompile += [
+  'application_new.css',
+  'application_admin.css',
+  'debug.css',
+  'sections/*.css',
+  'application_new.js',
+  'application_admin.js',
+  'sections/*.js',
+]
 Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
