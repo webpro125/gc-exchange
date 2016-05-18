82e3aebb5919fb166ae44ba723fb1b066aad3532
Jess Brown <jess@brownwebdesign.com>
2016-05-18 17:37:53 -0400

    allow cross domain to the geo users api

--- app/controllers/api/geo_users_controller.rb
+++ app/controllers/api/geo_users_controller.rb
@@ -1,6 +1,10 @@
 class Api::GeoUsersController < ApplicationController
   respond_to :json
   def index
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'GET'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
     @consultants = Consultant.approved.includes(:user, :address, :project_histories => [:positions]).where("addresses.latitude is not null").references(:address)
   end
 end
