f6cc394d2f20cc750aeb84886281e5202e0634e0
Jess Brown <jess@brownwebdesign.com>
2016-06-07 08:52:05 -0400

    add search query

--- app/controllers/api/geo_users_controller.rb
+++ app/controllers/api/geo_users_controller.rb
@@ -5,6 +5,12 @@ class Api::GeoUsersController < ApplicationController
     headers['Access-Control-Allow-Methods'] = 'GET'
     headers['Access-Control-Request-Method'] = '*'
     headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    @consultants = Consultant.approved.includes(:user, :address, :project_histories => [:positions]).where("addresses.latitude is not null").references(:address)
+    if params[:q].present?
+      search = Search.new(q: params[:q])
+      response = Consultant.search(SearchAdapter.new(search).to_query)
+      @consultants = response.records
+    else
+      @consultants = Consultant.approved.includes(:user, :address, :project_histories => [:positions]).where("addresses.latitude is not null").references(:address)
+    end
   end
 end
